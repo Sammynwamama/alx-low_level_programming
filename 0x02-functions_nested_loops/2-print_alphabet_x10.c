15409a9d034f87aa0931bf4853850d3e3ee3f639
Sammynwamama <Sammynwamama@gmail.com>
2022-09-13 15:06:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Check main 
+ * print_alphabet_x10 - Check main
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 0
@@ -19,4 +19,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-
