60cb567df34611ca5a938961e6a35220aeca3075
Sammynwamama <Sammynwamama@gmail.com>
2022-09-15 13:40:38 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,18 @@
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
-int _isupper(int letter)
-
+int _isupper(int c)
 {
+	char uppercase = 'A';
+	int isupper = 0;
 
-	return (letter >= 'A' && letter <= 'Z');
-
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+	if (c == uppercase)
+		{
+		isupper = 1;
+		break;
+		}
+	}
+	return (isupper);
 }
