b778a6a878347dabbb2b1d50c92e38bde0c3cd36
Sammynwamama <Sammynwamama@gmail.com>
2022-09-19 14:24:39 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,13 @@
 void rev_string(char *s)
 {
 	int counter = 0;
-
 	int i = 0;
 
 	char swap;
 
 	while (s[counter] != '\0')
 		counter++;
-		counter -= 1;
+	counter -= 1;
 
 	while (counter > i)
 	{
