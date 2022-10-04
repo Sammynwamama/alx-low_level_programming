96693d479788ba06ba18490ae60f43ceddd28386
Sammynwamama <Sammynwamama@gmail.com>
2022-10-04 15:18:40 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,17 +18,30 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	array = (int **)malloc(sizeof(int *) * height);
-
 	if (array == NULL)
 		return (NULL);
-	for (; i < height; i++)
-	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-			return (NULL);
+	{	for (; i < height; i++)
+		{
+			array[i] = (int *)malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < height; j++)
+					array[i][j] = 0;
+			}
+			else
+			{	while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}	
+		}	
+		return (array);
 	}
-	for (i = 0; i < height; i++)
-		for (; j < width; j++)
-			array[i][j] = 0;
-	return (array);
+	else
+	{
+		return (NULL);
+	}
 }
