12125a56d4cf0ec27ebb7b0eb2a6d8742136f987
Sammynwamama <sammynwamama@gmail.com>
2022-10-25 14:33:27 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,11 +6,8 @@
 
 /**
  * print_listint - A function that prints all the elements in the list
-
  * @h: A pointer to listint structure
-
  * Return: the number of nodes.
-
  */
 
 size_t print_listint(const listint_t *h)
